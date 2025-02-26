94106b39e9547bdf1ac84de3edee65cff5db9ba1
Archer <superkakanoob@gmail.com>
2025-02-26 13:39:04 +0700

    Added skeleton code for classes

---
+++ app/class.py
@@ -0,0 +1,60 @@
+class App:
+    pass
+
+class User:
+    pass
+
+class Promotion:
+    pass
+
+class Location:
+    pass
+
+class Order:
+    pass
+
+class Review:
+    pass
+
+class Restaurant:
+    pass
+
+class RestaurantCollection:
+    pass
+
+class Food:
+    pass
+
+class FoodOption:
+    pass
+
+class OptionChoice:
+    pass
+
+class FoodComment:
+    pass
+
+class SelectedFood:
+    pass
+
+class Cart:
+    pass
+
+class Payment:
+    pass
+
+class QRPayment(Payment):
+    pass
+
+class CashPayment(Payment):
+    pass
+
+class DeliveryOption:
+    pass
+
+class Order:
+    pass
+
+
+def create_instance():
+    pass
