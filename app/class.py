539d82e2d0f81e2c7b509782b0f7cf24d1d39d8a
Archer <superkakanoob@gmail.com>
2025-02-26 15:04:33 +0700

    Done with kaka's part

--- app/class.py
+++ app/class.py
@@ -1,15 +1,45 @@
 # TODO: kaka
 class App:
-    pass
+    def __init__(self, users, restaurants):
+        self.users = users
+        self.restaurants = restaurants
 
 class User:
-    pass
+    def __init__(self, user_id, name, username, password, carts, locations, user_order_history, promotions, reviews):
+        self.user_id = user_id
+        self.name = name
+        self.username = username
+        self.password = password
+        self.carts = carts
+        self.locations = locations
+        self.user_order_history = user_order_history
+        self.promotions = promotions
+        self.reviews = reviews
 
 class Promotion:
-    pass
+    def __init__(self, name, restaurant, promotion_code):
+        self.name = name
+        self.restaurant = restaurant
+        self.promotion_code = promotion_code
 
 class Location:
-    pass
+    def __init__(self, phone_number, location_name, address, note):
+        self.phone_number = phone_number
+        self.location_name = location_name
+        self.address = address
+        self.note = note
+
+class User:
+    def __init(self, user_id, name, username, password, carts, locations, user_order_history, promotions, reviews):
+        pass
+
+class Promotion:
+    def __init__(self, name, restaurant, promotion_code):
+        pass
+
+class Location:
+    def __init__(self, phone_number, location_name, address, note):
+        pass
 
 # TODO: guer
 class UserOrder:
