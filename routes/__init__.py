c2947c5e50e4682a83e2b613e96501697042897d
Archer <superkakanoob@gmail.com>
2025-03-07 14:41:28 +0700

    made changes to models.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,10 +2,7 @@ from . import order
 from . import login
 from . import address
 from . import order_confirmation
-<<<<<<< HEAD
 from . import edit_address
 from . import SelectedFood
 from . import food
-=======
 from . import promotion
->>>>>>> 319c965a0ceece5cc61bab95e979782c5bdef38e
